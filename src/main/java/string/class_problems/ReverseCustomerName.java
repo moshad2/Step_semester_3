47f3a265febb4be6b16648bf6ce837781341fd39
moshad2 <mohamedshadin412@gmail.com>
2026-09-19 09:40:43 +0530

    Add ReverseCustomerName class for reversing names

---
+++ src/main/java/string/class_problems/ReverseCustomerName.java
@@ -0,0 +1,21 @@
+package string.class_problems;
+public class ReverseCustomerName {
+
+    public static String reverseCustomerName(String customerName) {
+        char[] chars = customerName.toCharArray();
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = chars.length - 1; i >= 0; i--) {
+            reversed.append(chars[i]);
+        }
+        return reversed.toString();
+    }
+
+    public static void main(String[] args) {
+        String originalName = "Sunil";
+        String reversedName = reverseCustomerName(originalName);
+
+        System.out.println("Original Name: " + originalName);
+        System.out.println("Reversed Name: " + reversedName);
+    }
+}
